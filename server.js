ebeab2e1772dc86154f9eb5628ee8167b38049a0
Amy <wilford.amy@gmail.com>
2022-10-25 15:46:15 -0400

    adjusted folder structure and file templates

--- server.js
+++ server.js
@@ -3,21 +3,24 @@ const express = require('express');
 const session = require('express-session');
 const exphabs = require('express-handlebars');
 const routes = require('./controllers');
-// HELPERS?
+const helpers = require('./utils/helpers'); //to add
 
 const sequelize = require('./config/connection');
-// QUESTION - WHAT DOES THIS DO?
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// NEED TO ADD HELPERS BEFORE ADDING
-// const hbs = exphbs.create({ helpers });
+const hbs = exphabs.create({ helpers }); //to add
 
 const sess = {
-    secret: 'secretcode', //what are we supposed to write here?
-    cookie: {}, 
+    secret: 'secret',
+    cookie: {
+        maxAge: 300000, 
+        httpOnly: true, 
+        secure: false,
+        sameSite: 'strict'
+    }, 
     resave: false, 
     saveUninitialize: true, 
     store: new SequelizeStore({
@@ -39,6 +42,7 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
   });
+
 /*
 WHEN I click on the logout option in the navigation
 THEN I am signed out of the site
