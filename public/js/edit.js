7dfc65f06c593352ef1b200d0ba8cc9430e7badc
Amy <wilford.amy@gmail.com>
2022-10-27 00:40:46 -0400

    adjusted styles in edit form

--- public/js/edit.js
+++ public/js/edit.js
@@ -2,10 +2,13 @@ const editFormHandler = async (event) => {
   event.preventDefault();
   const post_id = document.querySelector('input[name="post-id"]').value;
   const post_title = document.querySelector('input[name="post-title"]').value;
+  const user_id = document.querySelector('input[name="user-id"]').value;
+
   const post_content = document.querySelector(
     'input[name="post-content"]'
   ).value;
-
+  console.log(user_id);
+  console.log(post_id);
   console.log(post_title);
   console.log(post_content);
 
@@ -14,6 +17,7 @@ const editFormHandler = async (event) => {
     body: JSON.stringify({
       post_title,
       post_content,
+      user_id,
     }),
     headers: {
       "Content-Type": "application/json",
